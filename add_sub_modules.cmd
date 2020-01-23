45f42647cc36ad9c9f070a83d53ae4e56e2408bd
Johnny KNOBLAUCH <johnny@beezup.com>
2020-01-23 18:08:35 +0100

    add vlingo-net submodules

---
+++ add_sub_modules.cmd
@@ -0,0 +1,19 @@
+git submodule add https://github.com/vlingo-net/vlingo-net-http vlingo-net-http
+git submodule add https://github.com/vlingo-net/vlingo-net-symbio-ado vlingo-net-symbio-ado
+git submodule add https://github.com/vlingo-net/vlingo-net-directory vlingo-net-directory
+
+git submodule add https://github.com/vlingo-net/vlingo-net-symbio vlingo-net-symbio
+git submodule add https://github.com/vlingo-net/vlingo-net-cluster vlingo-net-cluster
+
+
+git submodule add https://github.com/vlingo-net/vlingo-net-wire vlingo-net-wire
+
+git submodule add https://github.com/vlingo-net/vlingo-net-actors vlingo-net-actors
+
+git submodule add https://github.com/vlingo-net/vlingo-net-common vlingo-net-common
+git submodule add https://github.com/vlingo-net/vlingo-net-uuid vlingo-net-uuid
+
+git submodule add https://github.com/vlingo-net/vlingo-net-auth vlingo-net-auth
+
+
+
