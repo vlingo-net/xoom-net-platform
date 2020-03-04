a9bd5220002c0cc6a401be987c932b0897187658
Johnny KNOBLAUCH <johnny@beezup.com>
2020-03-04 20:11:37 +0100

    relative submodule

--- add_sub_modules.cmd
+++ add_sub_modules.cmd
@@ -1,19 +1,23 @@
-git submodule add https://github.com/vlingo-net/vlingo-net-http vlingo-net-http
-git submodule add https://github.com/vlingo-net/vlingo-net-symbio-ado vlingo-net-symbio-ado
-git submodule add https://github.com/vlingo-net/vlingo-net-directory vlingo-net-directory
+git submodule add ../vlingo-net-http vlingo-net-http
+git submodule add ../vlingo-net-symbio-ado vlingo-net-symbio-ado
+git submodule add ../vlingo-net-directory vlingo-net-directory
 
-git submodule add https://github.com/vlingo-net/vlingo-net-symbio vlingo-net-symbio
-git submodule add https://github.com/vlingo-net/vlingo-net-cluster vlingo-net-cluster
+git submodule add ../vlingo-net-symbio vlingo-net-symbio
+git submodule add ../vlingo-net-cluster vlingo-net-cluster
 
 
-git submodule add https://github.com/vlingo-net/vlingo-net-wire vlingo-net-wire
+git submodule add ../vlingo-net-wire vlingo-net-wire
 
-git submodule add https://github.com/vlingo-net/vlingo-net-actors vlingo-net-actors
+git submodule add ../vlingo-net-actors vlingo-net-actors
 
-git submodule add https://github.com/vlingo-net/vlingo-net-common vlingo-net-common
-git submodule add https://github.com/vlingo-net/vlingo-net-uuid vlingo-net-uuid
+git submodule add ../vlingo-net-common vlingo-net-common
+git submodule add ../vlingo-net-uuid vlingo-net-uuid
 
-git submodule add https://github.com/vlingo-net/vlingo-net-auth vlingo-net-auth
+git submodule add ../vlingo-net-auth vlingo-net-auth
+
+git submodule add ../vlingo-net-examples vlingo-net-examples
+
+
 
 
 
---
+++ set_sub_modules.cmd
@@ -0,0 +1,23 @@
+git submodule set-url vlingo-net-http ../vlingo-net-http
+git submodule set-url vlingo-net-symbio-ado ../vlingo-net-symbio-ado
+git submodule set-url vlingo-net-directory ../vlingo-net-directory
+
+git submodule set-url vlingo-net-symbio ../vlingo-net-symbio
+git submodule set-url vlingo-net-cluster ../vlingo-net-cluster
+
+
+git submodule set-url vlingo-net-wire ../vlingo-net-wire
+
+git submodule set-url vlingo-net-actors ../vlingo-net-actors
+
+git submodule set-url vlingo-net-common ../vlingo-net-common
+git submodule set-url vlingo-net-uuid ../vlingo-net-uuid
+
+git submodule set-url vlingo-net-auth ../vlingo-net-auth
+
+git submodule set-url vlingo-net-examples ../vlingo-net-examples
+
+
+
+
+
